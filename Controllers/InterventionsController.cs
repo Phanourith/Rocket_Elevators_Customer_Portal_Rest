1a245f58a5b19df0011333918dfa4915f7407c38
longviddd <73258079+longviddd@users.noreply.github.com>
2021-12-02 01:10:42 -0500

    finished

---
+++ Controllers/InterventionsController.cs
@@ -0,0 +1,74 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Rocket_Elevators_REST_API.Models;
+using Microsoft.AspNetCore.JsonPatch;
+
+namespace Rocket_Elevators_REST_API.Controllers{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InterventionsController : ControllerBase{
+        private readonly ApplicationContext _context;
+        public InterventionsController(ApplicationContext context)
+        {
+            _context = context;
+        }
+        [HttpGet("pending")]
+        public async Task<ActionResult<List<Intervention>>> getPendingInterventions(){
+            var interventions = await _context.interventions.Where(i => i.status == "Pending").ToListAsync();
+            if (interventions == null)
+            {
+                return NotFound();
+            }
+            return interventions;
+        }
+        [HttpGet("inprogress")]
+        public async Task<ActionResult<List<Intervention>>> getInProgressInterventions(){
+            var interventions = await _context.interventions.Where(i => i.status == "InProgress").ToListAsync();
+            if (interventions == null)
+            {
+                return NotFound();
+            }
+            return interventions;
+        }
+        [HttpGet("completed")]
+        public async Task<ActionResult<List<Intervention>>> getCompletedInterventions(){
+            var interventions = await _context.interventions.Where(i => i.status == "Completed").ToListAsync();
+            if (interventions == null)
+            {
+                return NotFound();
+            }
+            return interventions;
+        }
+        [HttpPut("inprogress/{id}")]
+        public async Task<IActionResult> UpdateStatusInProgress(int id){
+            var interventionToUpdate = await _context.interventions.FindAsync(id);
+            if(interventionToUpdate == null){
+                return NotFound($"Intervention with Id = {id} not found");
+            }
+            interventionToUpdate.status = "InProgress";
+            interventionToUpdate.start_intervention = DateTime.Now;
+            _context.interventions.Update(interventionToUpdate);
+            _context.SaveChanges();
+            return Content("Successfully updated status to In Progress " + interventionToUpdate.start_intervention);
+        }
+        [HttpPut("complete/{id}")]
+        public async Task<IActionResult> UpdateStatusComplete(int id){
+            var interventionToUpdate = await _context.interventions.FindAsync(id);
+            if(interventionToUpdate == null){
+                return NotFound($"Intervention with Id = {id} not found");
+            }
+            interventionToUpdate.status = "Completed";
+            interventionToUpdate.result = "Completed";
+            interventionToUpdate.end_intervention = DateTime.Now;
+            _context.interventions.Update(interventionToUpdate);
+            _context.SaveChanges();
+            return Content("Successfully updated status to In Progress " + interventionToUpdate.end_intervention);
+        }
+
+    }
+
+}
--- Models/Intervention.cs
+++ Models/Intervention.cs
@@ -1,17 +1,18 @@
 using System;
 namespace Rocket_Elevators_REST_API.Models
 {
-    public class Employee
+    public class Intervention
     {
+        public int id {get; set;}
         public int building_id { get; set; }
         public int author { get; set; }
         public int battery_id { get; set; }
-        public int column_id { get; set; }
-        public int elevator_id { get; set; }
+        public int? column_id { get; set; }
+        public int? elevator_id { get; set; }
         public int customer_id { get; set; }
-        public int employee_id { get; set; }
-        public Date start_intervention { get; set; }
-        public Date end_intervention { get; set; }
+        public int? employee_id { get; set; }
+        public DateTime? start_intervention { get; set; }
+        public DateTime? end_intervention { get; set; }
         public string result {get; set;}
         public string report {get; set;}
         public string status {get; set;}
