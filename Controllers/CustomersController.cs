a9ba6559efe78d892e98fc3f991bf1df0d0ada8e
longviddd <73258079+longviddd@users.noreply.github.com>
2021-12-15 01:42:55 -0500

    added elevator endpoint

--- Controllers/CustomersController.cs
+++ Controllers/CustomersController.cs
@@ -69,5 +69,30 @@ namespace Rocket_Elevators_REST_API.Controllers
                 return Content(returnJson, "application/json");
             }
         }
+        [HttpGet("{email}/elevators")]
+        public async Task<ActionResult> getElevators(String email){
+            string returnJson = "";
+            var customer = await _context.customers.Where(e => e.email_of_the_company_contact == email).FirstOrDefaultAsync();
+            if(customer == null){
+                return NotFound();
+            }
+            else{
+                var buildings = await _context.buildings.Where(b => b.customer_id == customer.id).ToListAsync();
+                foreach(var building in buildings){
+                    var batteries = await _context.batteries.Where(b => b.building_id == building.id).ToListAsync();
+                    foreach(var battery in batteries){
+                        var columns = await _context.columns.Where(c => c.battery_id == battery.id).ToListAsync();
+                        foreach(var column in columns){
+                            var elevators = await _context.elevators.Where(e => e.column_id == column.id).ToListAsync();
+                            foreach(var elevator in elevators){
+                                var jsonString = Newtonsoft.Json.JsonConvert.SerializeObject(elevator);
+                                returnJson = returnJson + jsonString;
+                            }
+                        }
+                    }
+                }
+                return Content(returnJson, "application/json");
+            }
+        }
     }
 }
